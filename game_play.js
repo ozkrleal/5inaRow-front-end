c09397a0ce9b0762abc8c0e01959d6d356bd89e2
nasattila <nas.attila@gmail.com>
2017-12-04 17:52:49 +0100

    commit sixteen
    polling

--- game_play.js
+++ game_play.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
     var y = Math.floor(event.offsetY * SIZE / context.canvas.height);
     var gameid = localStorage.getItem('5inaRow_gameid');
     var username = localStorage.getItem('5inaRow_username');
-    alert(x + ", " + y);
+    // alert(x + ", " + y);
     $.ajax({
       type: "put",
       url: "http://localhost:3100/api/game/update/",
@@ -38,69 +38,89 @@ $(document).ready(function() {
       }
     })
     .done(function(data) {
-      // place piece:
-      context.beginPath();
-      context.arc(x * context.canvas.width / SIZE + context.canvas.height / SIZE / 2,
-        y * context.canvas.height / SIZE + context.canvas.height / SIZE / 2,
-        RADIUS,
-        0,
-        2 * Math.PI,
-        false);
-      context.fillStyle = "blue";
-      context.fill();
-      context.stroke();
-      //
-      setTimeout(function() {
-        $.ajax({
-          type: "get",
-          url: "http://localhost:3100/api/game/poll/",
-          headers: {"Authorization": localStorage.getItem('5inaRow_token')},
-          dataType: "json",
-          contentType: "application/json",
-          complete: poll,
-          timeout: 5000,
-          data: {
-            "gameId": gameid
-          }
-        })
-        .done(function(data) {
-          var code = data.code;
-          // break if game over or this player's turn
-          switch(code)
-          {
-            case 7:
-              window.location.href = 'game_draw.html';
-            case 8:
-              // send to "game_lost.html" or "game_won.html"
-            case 9:
-              alert("It is not your turn! Please wait for the other player to make a move.");
-            case 10:
-              // place piece:
-              context.beginPath();
-              context.arc(x * context.canvas.width / SIZE + context.canvas.height / SIZE / 2,
-                y * context.canvas.height / SIZE + context.canvas.height / SIZE / 2,
-                RADIUS,
-                0,
-                2 * Math.PI,
-                false);
-              context.fillStyle = "red";
-              context.fill();
-              context.stroke();
-              //
-              break;
-          }
-        })
-        .fail(function (jqXHR, statusText, error) {
-          var code = jqXHR.status;
-          if (code == 0) {
-            message = "no connection"; }
-          else if (code == 404) {
-            message = "requested page not found: " + code; }
-          else {
-            message = "uncaught error: " + jqXHR.responseText; }
-          alert(message);
-        })
-      }, 500);
+      var code = data.code;
+      switch(code) {
+        case 2:
+          alert("There is already a piece there; please place a piece somewhere else!");
+          break;
+        case 7:
+          window.location.href = 'game_draw.html';
+          break;
+        case 8:
+          window.location.href = 'game_won.html';
+          break;
+        case 9:
+          alert("It is not your turn! Please wait for the other player to make a move.");
+          break;
+        case 6:
+          // place piece:
+          context.beginPath();
+          context.arc(data.x * context.canvas.width / SIZE + context.canvas.height / SIZE / 2,
+            data.y * context.canvas.height / SIZE + context.canvas.height / SIZE / 2,
+            RADIUS,
+            0,
+            2 * Math.PI,
+            false);
+          context.fillStyle = "blue";
+          context.fill();
+          context.stroke();
+          //
+          alert("It is not your turn any more; please wait for the other player to make a move!");
+          var timeout = setTimeout(function() {
+            $.ajax({
+              type: "get",
+              url: "http://localhost:3100/api/game/poll/",
+              headers: {"Authorization": localStorage.getItem('5inaRow_token')},
+              dataType: "json",
+              contentType: "application/json",
+              timeout: 5000,
+              data: {
+                "gameId": gameid
+              }
+            })
+            .done(function(data) {
+              var code = data.code;
+              // break if game over or this player's turn
+              switch(code) {
+                case 7:
+                  window.location.href = 'game_draw.html';
+                  clearTimeout(timeout);
+                  break;
+                case 8:
+                  window.location.href = 'game_lost.html';
+                  clearTimeout(timeout);
+                  break;
+                case 9:
+                  break;
+                case 10:
+                  // place piece:
+                  context.beginPath();
+                  context.arc(data.x * context.canvas.width / SIZE + context.canvas.height / SIZE / 2,
+                    data.y * context.canvas.height / SIZE + context.canvas.height / SIZE / 2,
+                    RADIUS,
+                    0,
+                    2 * Math.PI,
+                    false);
+                  context.fillStyle = "red";
+                  context.fill();
+                  context.stroke();
+                  //
+                  alert("It is your turn; please make a move!");
+                  clearTimeout(timeout);
+                  break; }
+            })
+            .fail(function (jqXHR, statusText, error) {
+              var code = jqXHR.status;
+              if (code == 0) {
+                message = "no connection"; }
+              else if (code == 404) {
+                message = "requested page not found: " + code; }
+              else {
+                message = "uncaught error: " + jqXHR.responseText; }
+              alert(message);
+            })
+          }, 500);
+        break; }
     })
     .fail(function (jqXHR, statusText, error) {
       var code = jqXHR.status;
