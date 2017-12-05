f8c198e9e64e3f86113204b78113827c0bb34afa
nasattila <nas.attila@gmail.com>
2017-12-05 22:49:45 +0100

    commit eighteen
    minor fixes

--- leaderboard.js
+++ leaderboard.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
-    $.ajax({
-      type: "get",
-      url: "http://localhost:3100/api/highScores/",
-      headers: {"Authorization": localStorage.getItem('5inaRow_token')},
-      dataType: "json"
-    })
-    .done(function(data) {
-        $.each(data, function (i, item) {
-            var row =
-              "<tr>" +
-                "<td>" + item[1] + "</td>" +
-                "<td>" + item[0] + "</td>" +
-              "</tr>";
-            $('#leaderboard').append(row);
-        });
-    })
-    .fail(function (jqXHR, statusText, error) {
-      var code = jqXHR.status;
-      if (code == 0) {
-        message = "no connection"; }
-      else if (code == 404) {
-        message = "requested page not found: " + code; }
-      else if (code == 489) {
-        message = jqXHR.responseJSON.msg + ": " + code;
-      window.location.href = 'index.html'; }
-      else if (code == 499) {
-        message = jqXHR.responseJSON.msg + ": " + code;
-      window.location.href = 'index.html'; }
-      else if (code == 502) {
-        message = jqXHR.responseJSON.msg + ": " + code; }
-      else {
-        message = "uncaught error: " + jqXHR.responseText; }
-      alert(message);
-    })
+  $.ajax({
+    type: "get",
+    url: "http://localhost:3100/api/highScores/",
+    headers: {"Authorization": localStorage.getItem('5inaRow_token')},
+    dataType: "json",
+    contentType: "application/json"
+  })
+  .done(function(data) {
+      $.each(data, function (i, item) {
+          var row =
+            "<tr>" +
+              "<td>" + item[1] + "</td>" +
+              "<td>" + item[0] + "</td>" +
+            "</tr>";
+          $('#leaderboard').append(row);
+      });
+  })
+  .fail(function (jqXHR, statusText, error) {
+    var code = jqXHR.status;
+    if (code == 0) {
+      message = "no connection"; }
+    else if (code == 404) {
+      message = "requested page not found: " + code; }
+    else if (code == 489) {
+      message = jqXHR.responseJSON.msg + ": " + code;
+    window.location.href = 'index.html'; }
+    else if (code == 499) {
+      message = jqXHR.responseJSON.msg + ": " + code;
+    window.location.href = 'index.html'; }
+    else if (code == 502) {
+      message = jqXHR.responseJSON.msg + ": " + code; }
+    else {
+      message = "uncaught error: " + jqXHR.responseText; }
+    alert(message);
   })
 });
--- load_header_in.js
+++ load_header_in.js
@@ -5,7 +5,8 @@ function logOut() {
   $.ajax({
     type: "get",
     url: "http://localhost:3100/api/user/logout/",
-    headers: {"Authorization": localStorage.getItem('5inaRow_token')}
+    headers: {"Authorization": localStorage.getItem('5inaRow_token')},
+    contentType: "application/json"
   })
   .done(function(data) {
     window.localStorage.removeItem('5inaRow_token');
