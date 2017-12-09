c3edf1ebee548d588b3940f783a9ec8206e2a2bd
nasattila <nas.attila@gmail.com>
2017-12-09 02:47:36 +0100

    commit twenty-six
    fixes

--- find_game.js
+++ find_game.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  var form = $('#findGame');
-  form.click(function(event) {
+  var findGame = $('#find_game');
+  findGame.click(function(event) {
     event.preventDefault();
+    findGame.prop('disabled', true);
     var result = {};
     $.ajax({
-      type: form.attr('method'),
-      url: form.attr('action'),
+      type: 'post',
+      url: "http://localhost:3100/api/game/",
       headers: {"Authorization": localStorage.getItem('5inaRow_token')},
       dataType: "json",
       contentType: "application/json",
@@ -14,6 +15,7 @@ $(document).ready(function() {
     .done(function(data) {
       if(data.success == true) {
         window.localStorage.setItem('5inaRow_gameid', data.gameId);
+        $('#find_game').prop('disabled', false);
         window.location.href = 'game_area.html'; }
       else {
         $('#find_game').prop('disabled', false);
@@ -22,8 +24,7 @@ $(document).ready(function() {
     .fail(function (jqXHR, statusText, error) {
       var code = jqXHR.status;
       if (code == 0) {
-        message = "no connection";
-        $('#find_game').prop('disabled', false); }
+        message = "no connection"; }
       else if (code == 404) {
         message = "requested page not found: " + code; }
       else if (code == 401) {
@@ -33,8 +34,7 @@ $(document).ready(function() {
         message = ": " + code;
         window.location.href = 'index.html'; }
       else if (code == 408) {
-        message = ": " + code;
-        $('#find_game').prop('disabled', false); }
+        message = ": " + code; }
       else if (code == 489) {
         message = ": " + code;
         window.location.href = 'index.html'; }
@@ -42,11 +42,10 @@ $(document).ready(function() {
         message = ": " + code;
         window.location.href = 'index.html'; }
       else if (code == 502) {
-        message = ": " + code;
-        $('#find_game').prop('disabled', false); }
+        message = ": " + code; }
       else {
-        message = "uncaught error: " + jqXHR.responseText;
-        $('#find_game').prop('disabled', false); }
+        message = "uncaught error: " + jqXHR.responseText; }
+      findGame.prop('disabled', false);
       alert(message);
     })
   })
